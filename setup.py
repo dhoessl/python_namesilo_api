2e987b9872b7ec923c81ca657323961ae1b0afb8
Dominic Hößl <dominic.hoessl@noris.de>
2025-04-18 15:57:09 +0200

    fix name

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name='lcd_i2c_display_matrix',
-    version='0.1',
+    name='namesilo_api',
+    version='0.1-0',
     description='A Python3 wrapper for namesilos API',
     url='https://github.com/dhoessl/python_namesilo_api',
     author="Dominic Hößl",
